b9523c95daca72b9fd93076ca918315e38994581
Sneha Poojary <snehapoojary2004@gmail.com>
2026-02-01 17:40:24 +0530

    Initial project setup with Maven, including core application files and configurations

---
+++ Backend/pulseguard/src/test/java/com/pulseguard/PulseGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulseguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
